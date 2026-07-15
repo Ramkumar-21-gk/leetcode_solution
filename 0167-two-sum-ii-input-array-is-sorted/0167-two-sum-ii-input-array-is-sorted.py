34b6af41fdd7953bad00233f80d3b9f242a6c1ce
Ramkumar Prajapati <gkramkumarprajapati49@gmail.com>
2026-07-15 23:09:10 +0530

    Time: 7 ms (50.65%), Space: 12.7 MB (82.92%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -2,18 +2,17 @@ class Solution(object):
     def twoSum(self, numbers, target):
         i=0
         j=len(numbers)-1
-
-        while i<j:
+        while i<=j:
             sum=numbers[i]+numbers[j]
-            if sum==target:
-                return (i+1,j+1)
-            
-            if sum>target:
-                j-=1
+
+            if target==sum:
+                return [i+1,j+1]
             elif sum<target:
                 i+=1
-            
+            else:
+                j-=1
         return -1
+
 
 obj=Solution()
 obj.twoSum([2,7,11,15],9)
