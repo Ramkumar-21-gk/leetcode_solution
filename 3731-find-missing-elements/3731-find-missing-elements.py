903a98ba97ab8860728f1422820f255ecf49d9b9
Ramkumar Prajapati <gkramkumarprajapati49@gmail.com>
2026-08-04 20:10:38 +0530

    Time: 3 ms (83.41%), Space: 12.5 MB (17.28%) - LeetHub

---
+++ 3731-find-missing-elements/3731-find-missing-elements.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findMissingElements(self, nums):
+        s = set(nums)
+        ans = []
+
+        for num in range(min(nums), max(nums) + 1):
+            if num not in s:
+                ans.append(num)
+
+        return ans
+        
