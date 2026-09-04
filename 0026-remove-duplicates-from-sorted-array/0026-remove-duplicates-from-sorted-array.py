39f70a3c1b2b40803584a9896677432a977805b5
Ramkumar Prajapati <gkramkumarprajapati49@gmail.com>
2026-09-04 19:11:58 +0530

    Time: 7 ms (19.19%), Space: 13.2 MB (93.14%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,17 +1,30 @@
 class Solution(object):
     def removeDuplicates(self, nums):
-        count=1
-        move=1
+        unique=1
+        officer=1
         cm=0
         n=len(nums)
-        while move<n:
-            if nums[move]==nums[cm]:
-                move+=1
-            else:
-                cm+=1
-                nums[count],nums[move]=nums[move],nums[count]
-                count+=1
-                move+=1
-        return count
+        while officer<n:
+            if nums[officer-1]==nums[officer]:
+                officer+=1
+                continue
+            cm+=1
+            nums[cm]=nums[officer]
+            unique+=1
+            officer+=1
+        return unique
+        # count=1
+        # move=1
+        # cm=0
+        # n=len(nums)
+        # while move<n:
+        #     if nums[move]==nums[cm]:
+        #         move+=1
+        #     else:
+        #         cm+=1
+        #         nums[count],nums[move]=nums[move],nums[count]
+        #         count+=1
+        #         move+=1
+        # return count
 
         
