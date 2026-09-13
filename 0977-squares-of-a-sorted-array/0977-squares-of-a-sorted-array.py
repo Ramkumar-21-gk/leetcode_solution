25d5c50928e92ed59dd7c179d63379512aecdf13
Ramkumar Prajapati <gkramkumarprajapati49@gmail.com>
2026-09-13 20:31:07 +0530

    Time: 19 ms (17.96%), Space: 14.2 MB (47.74%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,44 +1,87 @@
 class Solution:
     def sortedSquares(self, nums):
 
-        negative = []
-        positive = []
+        neg=[]
+        pos=[]
 
-        # Separate negative and positive
         for num in nums:
-            if num < 0:
-                negative.append(num * num)
+            if num<0:
+                neg.append(num*num)
             else:
-                positive.append(num * num)
-
-        # Negative squares are in decreasing order
-        # so reverse them
-        negative.reverse()
-
-        # Now both are sorted
-        i = 0
-        j = 0
-
-        result = []
-
-        # Merge two sorted arrays
-        while i < len(negative) and j < len(positive):
-
-            if negative[i] <= positive[j]:
-                result.append(negative[i])
-                i += 1
+                pos.append(num*num)
+        
+        neg.reverse()
+        i,j=0,0
+        result=[]
+        while i<len(neg) and j<len(pos):
+            if neg[i]<pos[j]:
+                result.append(neg[i])
+                i+=1
             else:
-                result.append(positive[j])
-                j += 1
+                result.append(pos[j])
+                j+=1
+        
+        while i<len(neg):
+            result.append(neg[i])
+            i+=1
 
-        # Remaining negative squares
-        while i < len(negative):
-            result.append(negative[i])
-            i += 1
-
-        # Remaining positive squares
-        while j < len(positive):
-            result.append(positive[j])
-            j += 1
-
+        while j<len(pos):
+            result.append(pos[j])
+            j+=1
+        
         return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # negative = []
+        # positive = []
+
+        # # Separate negative and positive
+        # for num in nums:
+        #     if num < 0:
+        #         negative.append(num * num)
+        #     else:
+        #         positive.append(num * num)
+
+        # # Negative squares are in decreasing order
+        # # so reverse them
+        # negative.reverse()
+
+        # # Now both are sorted
+        # i = 0
+        # j = 0
+
+        # result = []
+
+        # # Merge two sorted arrays
+        # while i < len(negative) and j < len(positive):
+
+        #     if negative[i] <= positive[j]:
+        #         result.append(negative[i])
+        #         i += 1
+        #     else:
+        #         result.append(positive[j])
+        #         j += 1
+
+        # # Remaining negative squares
+        # while i < len(negative):
+        #     result.append(negative[i])
+        #     i += 1
+
+        # # Remaining positive squares
+        # while j < len(positive):
+        #     result.append(positive[j])
+        #     j += 1
+
+        # return result
