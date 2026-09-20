e8536f274025b40db8bd4eec23544dca2ca04ff6
Ramkumar Prajapati <gkramkumarprajapati49@gmail.com>
2026-09-20 23:43:05 +0530

    Time: 7 ms (91.14%), Space: 12.4 MB (22.88%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def reverseDegree(self, s):
+        ans = 0
+
+        for i, ch in enumerate(s):
+            value = 26 - (ord(ch) - ord('a'))
+            ans += value * (i + 1)
+
+        return ans
